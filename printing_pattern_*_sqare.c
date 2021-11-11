5a6bbaa55ab94875d5d9deee60fc4bc817a523cd
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-11 12:45:16 +0530

    Add files via upload

---
+++ printing_pattern_*_sqare.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int row,col,n;
+    printf("enter the number of rows:");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n;col++)
+        {
+        if(row==1 || row==n ||col==1 || col==n)
+            printf("*");
+        else
+            printf(" ");
+        }
+        
+        printf("\n");
+    }
+    
+
+    return 0;
+}
+
+
