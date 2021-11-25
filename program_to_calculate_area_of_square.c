59c22d1e567798de76fb4defe00b3c032e9aa5c5
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-25 23:25:17 +0530

    area of square

---
+++ program_to_calculate_area_of_square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float area,n;
+    printf("Enter the length of square:");
+    scanf("%f",&n);
+    area=n*n;
+    printf("The area of the square is:%f\n",area);
+}
