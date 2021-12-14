c80c6c15736c17fd78a79325901fc5ed36016c29
datta kakde <85458867+datta184@users.noreply.github.com>
2021-12-14 08:24:55 +0530

    odd even number without modulo

---
+++ odd_even_without_modulo.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter the numbner:");
+    scanf("%d",&a);
+    if(a&&1==0)
+    printf("the number %d is even number:",a);
+    else
+    printf("the number %d is not even number:",a);
+
+    return 0;
+}
